79ed6e0421f900875e77e2808b749db11b935382
Abubakar Baba <4821saddikhady@gmail.com>
2026-08-07 23:39:48 +0300

    bind

--- phase-01-js-foundations/01-exercises/09-this-keyword.js
+++ phase-01-js-foundations/01-exercises/09-this-keyword.js
@@ -22,4 +22,25 @@ console.log("car info :", car.showInfo());
 car.refuel(20);
 console.log("car info :", car.showInfo());
 
+function buy(product, price) {
+  console.log(`${this.name} bought ${product} for $${price}`);
+}
 
+const customer = {
+  name: "Sadik",
+};
+
+buy.apply(customer, ["Laptop", 1000]);
+
+function showBalance() {
+  console.log(`Account name: ${this.name} \nBalance: $${this.balance}`);
+}
+
+const account = {
+  name: "Sadik",
+  balance: 100,
+};
+
+const showBalanceFn = showBalance.bind(account);
+
+showBalanceFn();
