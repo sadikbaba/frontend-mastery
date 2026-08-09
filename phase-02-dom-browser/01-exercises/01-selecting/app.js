ebfc0ca5426a77b30047190061d5e028ad032065
Abubakar Baba <4821saddikhady@gmail.com>
2026-08-10 02:35:11 +0300

    Complete DOM selecting exercise

--- phase-02-dom-browser/01-exercises/01-selecting/app.js
+++ phase-02-dom-browser/01-exercises/01-selecting/app.js
@@ -2,6 +2,10 @@ const title = document.querySelector("#title");
 const paragraph = document.querySelector(".text");
 const button = document.querySelector("#btn");
 const box = document.querySelector(".box");
+const paragraphs = document.querySelectorAll(".text");
+
+console.log(paragraphs);
 
 title.textContent = "DOM Mastery"; 
 
+box.innerHTML = "<strong>JavaScript changed this!</strong>";
