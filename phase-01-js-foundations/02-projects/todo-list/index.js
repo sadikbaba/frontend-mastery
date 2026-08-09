cfe57a8968a06d3b183ccb233ce104b599971a03
Abubakar Baba <4821saddikhady@gmail.com>
2026-08-09 11:44:49 +0300

    add todo lookup

--- phase-01-js-foundations/02-projects/todo-list/index.js
+++ phase-01-js-foundations/02-projects/todo-list/index.js
@@ -16,8 +16,23 @@ function addTodo(title) {
 function getTodos() {
   return [...todos];
 }
+
+function findTodo(id) {
+  const todo = todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    throw Error("Todo not found");
+  }
+  else {
+    return todo;
+  }
+
+}
+
 addTodo("Learn JavaScript");
 addTodo("Learn React");
 addTodo("Learn Vue");
-
 console.log(getTodos());
+
+console.log("\n____find todo____");
+console.log("your todo is: ", findTodo(77));
