fbff3ec521c958046b89a7813871f64efed752c3
Abubakar Baba <4821saddikhady@gmail.com>
2026-08-10 02:02:20 +0300

    learn changing DOM content

--- phase-02-dom-browser/01-exercises/01-selecting/app.js
+++ phase-02-dom-browser/01-exercises/01-selecting/app.js
@@ -1,29 +1,7 @@
 const title = document.querySelector("#title");
+const paragraph = document.querySelector(".text");
 const button = document.querySelector("#btn");
+const box = document.querySelector(".box");
 
-title.textContent = "I control the DOM";
-title.style.color = "darkblue";
-title.style.fontSize = "28px";
+title.textContent = "DOM Mastery"; 
 
-button.style.backgroundColor = "green";
-button.style.color = "white";
-button.style.padding = "10px 20px";
-button.style.border = "none";
-button.style.borderRadius = "16px";
-
-button.style.transition = "background-color 0.3s ease";
-
-button.addEventListener("click", function () {
-  console.log("Button was clicked");
-  title.textContent = "You clicked the button!";
-  title.style.color = "green";
-  button.style.backgroundColor = "red";
-  button.textContent = " clicked";
-
-  const addParagraph = document.createElement("p");
-
-  addParagraph.textContent = "I was created with JavaScript";
-  addParagraph.style.color = "darkblue";
-
-  document.body.appendChild(addParagraph);
-});
